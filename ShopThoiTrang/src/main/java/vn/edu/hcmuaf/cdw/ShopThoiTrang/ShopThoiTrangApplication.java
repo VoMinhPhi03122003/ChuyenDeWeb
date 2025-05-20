6e212b614dc87fda063623b16957edc80681b342
Lâm Hồng Phúc <144334980+PhucJames@users.noreply.github.com>
2025-05-20 22:51:17 +0700

    setup project

---
+++ ShopThoiTrang/src/main/java/vn/edu/hcmuaf/cdw/ShopThoiTrang/ShopThoiTrangApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.cdw.ShopThoiTrang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopThoiTrangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopThoiTrangApplication.class, args);
+    }
+
+}
---
+++ ShopThoiTrang/src/test/java/vn/edu/hcmuaf/cdw/ShopThoiTrang/ShopThoiTrangApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.cdw.ShopThoiTrang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopThoiTrangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
